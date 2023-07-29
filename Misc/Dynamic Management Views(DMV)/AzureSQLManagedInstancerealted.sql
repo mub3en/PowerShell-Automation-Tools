ac1adcef4bb2a31d7986fd0f92cf0b1d84853b33
Mubeen <hafizalimubeen@gmail.com>
2023-07-29 00:31:07 -0400

    Update AzureSQLManagedInstancerealted.sql

--- Misc/Dynamic Management Views(DMV)/AzureSQLManagedInstancerealted.sql
+++ Misc/Dynamic Management Views(DMV)/AzureSQLManagedInstancerealted.sql
@@ -449,4 +449,68 @@ GROUP BY convert(date, start_time)
 ORDER BY convert(date, start_time);
 GO
 
+-------------------------------
+--Maximum concurrent requests--
+-------------------------------
+SELECT COUNT(*) AS [Concurrent_Requests]
+FROM sys.dm_exec_requests R;
 
+SELECT COUNT(*) AS [Concurrent_Requests]
+FROM sys.dm_exec_requests R
+INNER JOIN sys.databases D ON D.database_id = R.database_id
+AND D.name = 'MyDatabase';
+
+
+-----------------------------
+--Maximum concurrent logins--
+-----------------------------
+
+--Maximum sessions
+SELECT COUNT(*) AS [Sessions]
+FROM sys.dm_exec_connections;
+
+SELECT COUNT(*) AS [Sessions]
+FROM sys.dm_exec_connections C
+INNER JOIN sys.dm_exec_sessions S ON (S.session_id = C.session_id)
+INNER JOIN sys.databases D ON (D.database_id = S.database_id)
+WHERE D.name = 'MyDatabase';
+
+
+--------------------------------
+--Monitoring query performance--
+--------------------------------
+
+--Finding top N queries
+SELECT TOP 5 query_stats.query_hash AS "Query Hash",
+    SUM(query_stats.total_worker_time) / SUM(query_stats.execution_count) AS "Avg CPU Time",
+     MIN(query_stats.statement_text) AS "Statement Text"
+FROM
+    (SELECT QS.*,
+        SUBSTRING(ST.text, (QS.statement_start_offset/2) + 1,
+            ((CASE statement_end_offset
+                WHEN -1 THEN DATALENGTH(ST.text)
+                ELSE QS.statement_end_offset END
+            - QS.statement_start_offset)/2) + 1) AS statement_text
+FROM sys.dm_exec_query_stats AS QS
+CROSS APPLY sys.dm_exec_sql_text(QS.sql_handle) as ST) as query_stats
+GROUP BY query_stats.query_hash
+
+--Monitoring query plans
+  SELECT
+    highest_cpu_queries.plan_handle,
+    highest_cpu_queries.total_worker_time,
+    q.dbid,
+    q.objectid,
+    q.number,
+    q.encrypted,
+    q.[text]
+FROM
+    (SELECT TOP 50
+        qs.plan_handle,
+        qs.total_worker_time
+    FROM
+        sys.dm_exec_query_stats qs
+ORDER BY qs.total_worker_time desc) AS highest_cpu_queries
+CROSS APPLY sys.dm_exec_sql_text(plan_handle) AS q
+ORDER BY highest_cpu_queries.total_worker_time DESC;
+ORDER BY 2 DESC;
