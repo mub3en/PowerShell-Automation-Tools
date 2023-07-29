fd0b60a2aee4a9fa6a1758f1be77543ff68225c7
Mubeen <hafizalimubeen@gmail.com>
2023-07-28 23:57:54 -0400

    Create AzureSQLDBRelated.sql

---
+++ Misc/Dynamic Management Views(DMV)/AzureSQLDBRelated.sql
@@ -0,0 +1,14 @@
+/*
+Ref article: https://learn.microsoft.com/en-us/azure/azure-sql/database/high-cpu-diagnose-troubleshoot?view=azuresql-db
+
+Diagnose and troubleshoot high CPU on Azure SQL Database
+
+*/
+
+--Identify vCore count with Transact-SQL
+SELECT 
+    COUNT(*) as vCores
+FROM sys.dm_os_schedulers
+WHERE status = N'VISIBLE ONLINE';
+GO
+
