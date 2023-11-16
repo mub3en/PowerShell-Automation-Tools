3b3720188076ce1f4c3896fcb8f8c5477874a0de
Mubeen <hafizalimubeen@gmail.com>
2023-11-16 14:01:52 -0500

    Re-formatting sample_report_2.sql

--- SQL/src/Scripts/SP/sample_report_2.sql
+++ SQL/src/Scripts/SP/sample_report_2.sql
@@ -1,25 +1,23 @@
 --Database File Location Summary
-
-If exists(Select 1 from sys.objects where name = 'getDBFileLocationInfo' and type = 'P')
+IF EXISTS( Select 1 FROM sys.objects WHERE name = 'getDBFileLocationInfo' AND type = 'P') 
 DROP PROCEDURE [dbo].[getDBFileLocationInfo]
 GO
-CREATE PROCEDURE [dbo].[getDBFileLocationInfo]
-AS
+
+CREATE PROCEDURE [dbo].[getDBFileLocationInfo] AS 
 BEGIN
-SELECT distinct
-   DB_NAME(database_id) [Database name],
-   type_desc [Database file Type],
-   name [File name],
-   physical_name [File Location],
-   state_desc [Database file status],
-   SIZE [Initial Size (MB)],
-   max_size [Maximum Size (MB)]
-FROM sys.master_files masterfiles WHERE DB_NAME(database_id)
-NOT IN
-('master','msdb','tempdb','model')
-
-
+    SELECT
+        distinct DB_NAME(database_id)   AS [Database name],
+            type_desc                   AS [Database file Type],
+            name                        AS [File name],
+            physical_name               AS [File Location],
+            state_desc                  AS [Database file status],
+            SIZE                        AS [Initial Size (MB)],
+            max_size                    AS [Maximum Size (MB)]
+    FROM
+        sys.master_files masterfiles
+    WHERE
+        DB_NAME(database_id) NOT IN ('master', 'msdb', 'tempdb', 'model')
 END
 GO
-
-exec dbo.getDBFileLocationInfo
+    
+EXEC dbo.getDBFileLocationInfo
